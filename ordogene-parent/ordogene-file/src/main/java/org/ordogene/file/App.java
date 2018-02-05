36a69dc874160ff7354cf2ce488f064ff6646163
ordogene <ordogene@lastproject>
2018-02-05 11:36:38 +0100

    initial commit

---
+++ ordogene-parent/ordogene-file/src/main/java/org/ordogene/file/App.java
@@ -0,0 +1,13 @@
+package org.ordogene.file;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
