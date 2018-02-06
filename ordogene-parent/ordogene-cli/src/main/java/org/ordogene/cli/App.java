8dac32617e4c531a17eca861b1b7423f4bc276a7
ordogene <ordogene@lastproject>
2018-02-06 11:04:49 +0100

    added documentation of Commands, added parameters validation

--- ordogene-parent/ordogene-cli/src/main/java/org/ordogene/cli/App.java
+++ ordogene-parent/ordogene-cli/src/main/java/org/ordogene/cli/App.java
@@ -4,7 +4,6 @@ import org.jline.utils.AttributedString;
 import org.jline.utils.AttributedStyle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.shell.jline.PromptProvider;
 
