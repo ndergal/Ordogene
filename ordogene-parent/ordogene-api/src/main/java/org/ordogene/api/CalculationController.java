855dd6a6f55a81c9ae0981972fe4c6b80d5d478e
ordogene <ordogene@lastproject>
2018-02-08 17:56:50 +0100

    lancement d'un calcul (expérimental)

--- ordogene-parent/ordogene-api/src/main/java/org/ordogene/api/CalculationController.java
+++ ordogene-parent/ordogene-api/src/main/java/org/ordogene/api/CalculationController.java
@@ -1,7 +1,11 @@
 package org.ordogene.api;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.ordogene.file.Const;
 import org.ordogene.file.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,6 +20,9 @@ public class CalculationController {
 
 	@Autowired
 	FileService fs;
+
+	private final Map<Integer, String> currentCalculation = new HashMap<>();
+	private final Object token = new Object();
 
 	@RequestMapping(value = "/{id}/calculations")
 	@ResponseBody
@@ -37,4 +44,60 @@ public class CalculationController {
 		}
 	}
 
+	@RequestMapping(value = "/launchCalculation/{id}")
+	@ResponseBody
+	public ResponseEntity<String> launchCalculation(@PathVariable String id) {
+
+		if (id == null) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(id + " does not exist");
+		}
+
+		boolean exist = fs.userExist(id);
+
+		if (!exist) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(id + " does not exist");
+		}
+
+		int pid = 0;
+		try {
+			String command = "java -jar " + Const.getConst().get("JarAlgorithmPath");
+			Process proc = Runtime.getRuntime().exec(command);
+			Field f = proc.getClass().getDeclaredField("pid");
+			f.setAccessible(true);
+			pid = (int) f.get(proc);
+			synchronized (token) {
+				currentCalculation.put(pid, id);
+			}
+			asynchronousDeletePid(proc, pid, id);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Unknow error... Sorry.");
+		}
+		return ResponseEntity.ok().body("" + pid);
+	}
+
+	private void asynchronousDeletePid(Process proc, int pid, String id) {
+
+		Runnable waitAndDeletePid = new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					// int exitVal = proc.waitFor();
+					int exitVal = proc.waitFor();
+					synchronized (token) {
+						if (currentCalculation.containsKey(pid))
+							currentCalculation.remove(pid);
+					}
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+		};
+
+		Thread t = new Thread(waitAndDeletePid);
+		t.setDaemon(true);
+		t.start();
+	}
 }
