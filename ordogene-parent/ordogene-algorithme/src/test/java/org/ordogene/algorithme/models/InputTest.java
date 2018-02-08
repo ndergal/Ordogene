d2b17e36f9e070904d41c26c5512c193d87d3a92
ordogene <ordogene@lastproject>
2018-02-08 15:16:38 +0100

    Add InputTest

---
+++ ordogene-parent/ordogene-algorithme/src/test/java/org/ordogene/algorithme/models/InputTest.java
@@ -0,0 +1,34 @@
+package org.ordogene.algorithme.models;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InputTest {
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInput() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateInput() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRelation() {
+		fail("Not yet implemented");
+	}
+
+}
