50e2e7444d2357fd6d68f846be24c4d147da307b
ordogene <ordogene@lastproject>
2018-02-09 16:27:55 +0100

    replace profiles with cmd line arguments for server url, remove useless propertie files

--- ordogene-parent/ordogene-cli/src/main/java/org/ordogene/cli/App.java
+++ ordogene-parent/ordogene-cli/src/main/java/org/ordogene/cli/App.java
@@ -2,25 +2,22 @@ package org.ordogene.cli;
 
 import org.jline.utils.AttributedString;
 import org.jline.utils.AttributedStyle;
-import org.springframework.beans.factory.annotation.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
 import org.springframework.shell.jline.PromptProvider;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@Configuration
-@PropertySource("classpath:ordogene-${spring.profiles.active}.properties")
-public class App 
+public class App
 {    
-	@Value("${api.root}")
-	private String addr;
-
+	private static String addr;
+	private static final Logger log = LoggerFactory.getLogger(App.class);
+	
     @Bean
 	public PromptProvider myPromptProvider() {
 		return () -> new AttributedString("ordogene:>", AttributedStyle.DEFAULT.foreground(AttributedStyle.YELLOW));
@@ -31,13 +28,18 @@ public class App
     	return builder.rootUri(addr).build();
     }
     
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-    	return new PropertySourcesPlaceholderConfigurer();
-    }
-    
     public static void main( String[] args )
     {
+    	SimpleCommandLinePropertySource ps = new SimpleCommandLinePropertySource(args);
+    	if(!ps.containsProperty("url")) {
+    		log.error("Missing argument --url=<server_url>");
+    		return;
+    	}
+    	addr = (String) ps.getProperty("url");
+    	if(addr.isEmpty()) {
+    		log.error("URL is empty");
+    		return;
+    	}
     	SpringApplication.run(App.class, args);
     }
 }
