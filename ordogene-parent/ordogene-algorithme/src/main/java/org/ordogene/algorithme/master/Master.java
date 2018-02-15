bfd10ea44f820fae38356fbf8858ef87f93aa821
ordogene <ordogene@lastproject>
2018-02-15 10:17:45 +0100

    Resolve conflict

--- ordogene-parent/ordogene-algorithme/src/main/java/org/ordogene/algorithme/master/Master.java
+++ ordogene-parent/ordogene-algorithme/src/main/java/org/ordogene/algorithme/master/Master.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -17,6 +16,7 @@ import java.util.concurrent.BlockingQueue;
 import javax.xml.bind.UnmarshalException;
 
 import org.ordogene.algorithme.Model;
+import org.ordogene.file.FileService;
 import org.ordogene.file.JSONModel;
 import org.ordogene.file.parser.Parser;
 import org.ordogene.file.utils.Calculation;
@@ -33,34 +33,6 @@ public class Master {
 	private final int maxThread;
 	private int currentThread;
 	private final Map<Integer, ThreadHandler> threadMap = new HashMap<>();
-	private final SimpleDateFormat formater = new SimpleDateFormat("dd/MM/yyyy-hh:mm");
-
-	static class ThreadHandler {
-		private Thread thread;
-		private final BlockingQueue<String> queue1 = new ArrayBlockingQueue<>(1);
-		private final BlockingQueue<String> queue2 = new ArrayBlockingQueue<>(1);
-
-		public void masterToThread(String str) throws InterruptedException {
-			queue1.put(str);
-		}
-
-		public String threadFromMaster() throws InterruptedException {
-			return queue1.poll();
-		}
-
-		public String masterFromThread() throws InterruptedException {
-			return queue2.take();
-		}
-
-		public void threadToMaster(String str) throws InterruptedException {
-			queue2.put(str);
-		}
-
-		public void setThread(Thread thread) {
-			this.thread = thread;
-		}
-
-	}
 
 	public Master() {
 		maxThread = DEFAULT_THREAD;
@@ -87,81 +59,23 @@ public class Master {
 
 		String toHash = jsonString + (new Date()).toString();
 		int numCalc = toHash.hashCode();
-
-
+		
 		ThreadHandler th = new ThreadHandler();
 
 		Thread t = new Thread(() -> {
 
-			Date currentDate = new Date();
-			int occur = 0;
-			int maxIter = model.getExecTime();
-			boolean interupted = false;
-			while (occur < maxIter && !interupted) {
-				System.out.println("hello world !(" + Thread.currentThread().getName() + ")");
-
-				// TODO call real algorithm functions
-				try {
-					Dummy.fakeCalculation(th, model.getName(), idUser, numCalc, occur);
-				} catch (InterruptedException | IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-
-				occur++;
-
-				try {
-					String str = th.threadFromMaster();
-					if (str != null && str.equals("state")) {
-						Random rand = RandomRegistry.getRandom();
-						int lastIterationSaved = rand.nextInt(occur); //TODO change RANDOM
-						StringBuilder sb = new StringBuilder();
-						sb.append(currentDate.getTime()).append("_");
-						sb.append(occur).append("_");
-						sb.append(lastIterationSaved).append("_");
-						sb.append(maxIter).append("_");
-						sb.append(rand.nextInt(Integer.MAX_VALUE));
-						th.threadToMaster(sb.toString());
-					} else if (str != null && str.equals("interrupt")) {
-						interupted = true;
-
-					}
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
- 			Random rand = RandomRegistry.getRandom();  //TODO change RANDOM
-			int lastIterationSaved = rand.nextInt(occur);
-			Calculation tmpCalc = new Calculation();
-			tmpCalc.setDate(currentDate);
-			tmpCalc.setIterationNumber(occur);
-			tmpCalc.setLastIterationSaved(lastIterationSaved);
-			tmpCalc.setMaxIteration(maxIter);
-			tmpCalc.setId(numCalc);
-			tmpCalc.setName(model.getName());
-			try {
-				String calculationSaveDest = Const.getConst().get("ApplicationPath") + File.separator + idUser
-						+ File.separator + tmpCalc.getId() + "_"+model.getName() + File.separatorChar + "state.json"; 
-				writeInFile(tmpCalc, Paths.get(calculationSaveDest));
-				System.out.println(tmpCalc + " saved in " + calculationSaveDest);
-			} catch (IOException e) {
-				e.printStackTrace();
-				System.err.println(tmpCalc + " not saved.");
-			}
-			// TODO donner blockingqueue a la méthode sunchronized pour get th
-			synchronized (threadMap) {
-				currentThread--;
-				threadMap.remove(numCalc);
-				return;
-			}
+			
 		});
+		
+
 		synchronized (threadMap) {
 			th.setThread(t);
 			t.start();
 			threadMap.put(numCalc, th);
 			return numCalc;
 		}
+
+		
 	}
 
 	public String getInfoByNumCalc(int numCalc) throws InterruptedException {
@@ -170,23 +84,8 @@ public class Master {
 		return th.masterFromThread();
 	}
 
-	private static void writeInFile(Object content, Path dest) throws IOException {
-
-		if (Files.exists(dest)) {
-			Files.delete(dest);
-		}
-		Files.createDirectories(dest.getParent());
-		Files.createFile(dest);
-
-		ObjectMapper mapper = new ObjectMapper();
-
-		// Object to JSON in file
-		mapper.writeValue(dest.toFile(), content);
-
-	}
-
 	// TODO connection with Thread
-	public void updateCalculation(Calculation cal) {
+	public void updateCalculation(Calculation cal, String userId) {
 		ThreadHandler th = threadMap.get(cal.getId());
 		if (th != null) {
 			// IsRunning
@@ -196,13 +95,12 @@ public class Master {
 				//Format: epoch_iterationNumber_lastIterationSaved_maxIteration_fitness
 				String[] state = th.masterFromThread().split("_");
 				try {
-					Date date = new Date(Long.valueOf(state[0]));
-					cal.setDate(formater.format(date));
+					cal.setStartTimestamp(Long.valueOf(state[0]));
 					cal.setIterationNumber(Integer.valueOf(state[1]));
 					cal.setLastIterationSaved(Integer.valueOf(state[2]));
 					cal.setMaxIteration(Integer.valueOf(state[3]));
 					cal.setFitnessSaved(Integer.valueOf(state[4]));
-				} catch (NumberFormatException | ArrayIndexOutOfBoundsException | ParseException e) {
+				} catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
 					throw new InternalError("Problem with calculation format informations");
 				}
 				cal.setRunning(true);
@@ -212,6 +110,18 @@ public class Master {
 			}
 			cal.setRunning(true);
 		} else {
+			try {
+				String pathName = Const.getConst().get("ApplicationPath") + File.separator + userId
+						+ File.separator + cal.getId() + "_" + cal.getName() + File.separatorChar + "state.json";
+				Calculation tmpCal = (Calculation) Parser.parseJsonFile(Paths.get(pathName), Calculation.class);
+				cal.setStartTimestamp(tmpCal.getStartTimestamp());
+				cal.setIterationNumber(tmpCal.getIterationNumber());
+				cal.setLastIterationSaved(tmpCal.getLastIterationSaved());
+				cal.setMaxIteration(tmpCal.getMaxIteration());
+				cal.setFitnessSaved(tmpCal.getFitnessSaved());
+			} catch (IllegalAccessException | UnmarshalException | IOException e) {
+				System.err.println("Problem to read the calculation ");
+			}
 			cal.setRunning(false);
 		}
 	}
