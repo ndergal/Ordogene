71907c40b624c33837eb808fba8b06d16f2a6d2c
ordogene <ordogene@lastproject>
2018-02-22 09:49:17 +0100

    change test and remove unused class

--- ordogene-parent/ordogene-algorithme/src/test/java/org/ordogene/algorithme/jenetics/CalculationHandlerTest.java
+++ ordogene-parent/ordogene-algorithme/src/test/java/org/ordogene/algorithme/jenetics/CalculationHandlerTest.java
@@ -38,7 +38,7 @@ public class CalculationHandlerTest {
 	// @Ignore
 	@Test
 	public void testCalculationHandler() throws Exception {
-		// RandomRegistry.setRandom(new Random(0));
+		RandomRegistry.setRandom(new Random(0));
 		URL urlTestFile = CalculationHandlerTest.class.getClassLoader()
 				.getResource("OrdogeneCalculationExamples" + File.separator + "short_path_10.json");
 		byte[] contentFile = Files.readAllBytes(Paths.get(urlTestFile.toURI()));
