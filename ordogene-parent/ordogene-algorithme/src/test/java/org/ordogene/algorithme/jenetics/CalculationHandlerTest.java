528595865d566afa94fdd6d5c88988d9311a0935
ordogene <ordogene@lastproject>
2018-02-22 09:32:05 +0100

    add init on test

--- ordogene-parent/ordogene-algorithme/src/test/java/org/ordogene/algorithme/jenetics/CalculationHandlerTest.java
+++ ordogene-parent/ordogene-algorithme/src/test/java/org/ordogene/algorithme/jenetics/CalculationHandlerTest.java
@@ -1,11 +1,13 @@
 package org.ordogene.algorithme.jenetics;
 
 import java.io.File;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Random;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
@@ -13,24 +15,37 @@ import org.ordogene.algorithme.Model;
 import org.ordogene.algorithme.master.ThreadHandler;
 import org.ordogene.file.JSONModel;
 import org.ordogene.file.parser.Parser;
+import org.ordogene.file.utils.Const;
 
 import io.jenetics.util.RandomRegistry;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CalculationHandlerTest {
-	
-private static final String userId = "tester";
 
-//	@Ignore
+	private static final String userId = "tester";
+
+	@Before
+	public void init() throws URISyntaxException {
+		String configFileLocation = CalculationHandlerTest.class.getClassLoader().getResource("ordogene.conf.json").toURI()
+				.toString();
+		if (configFileLocation.startsWith("file:")) {
+			configFileLocation = configFileLocation.substring(5);
+
+		}
+		Const.loadConfig(configFileLocation);
+	}
+
+	// @Ignore
 	@Test
 	public void testCalculationHandler() throws Exception {
-		//RandomRegistry.setRandom(new Random(0));
+		// RandomRegistry.setRandom(new Random(0));
 		URL urlTestFile = CalculationHandlerTest.class.getClassLoader()
 				.getResource("OrdogeneCalculationExamples" + File.separator + "short_path_10.json");
 		byte[] contentFile = Files.readAllBytes(Paths.get(urlTestFile.toURI()));
 		String jsonContent = new String(contentFile);
-		
-		CalculationHandler sb = new CalculationHandler(new ThreadHandler(), Model.createModel((JSONModel) Parser.parseJsonFile(jsonContent, JSONModel.class)), userId, 0);
+
+		CalculationHandler sb = new CalculationHandler(new ThreadHandler(),
+				Model.createModel((JSONModel) Parser.parseJsonFile(jsonContent, JSONModel.class)), userId, 0);
 		sb.launchCalculation();
 	}
 }
