99bfaa0e9ca79280ad43f1d7183879ff1ab2ec6d
ordogene <ordogene@lastproject>
2018-03-06 18:01:31 +0100

    remove unused methods

--- ordogene-parent/ordogene-algorithme/src/main/java/org/ordogene/algorithme/master/ThreadHandler.java
+++ ordogene-parent/ordogene-algorithme/src/main/java/org/ordogene/algorithme/master/ThreadHandler.java
@@ -4,13 +4,11 @@ package org.ordogene.algorithme.master;
 import java.util.Objects;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.TimeUnit;
 
 import org.ordogene.file.utils.Calculation;
 
 public class ThreadHandler {
 		private final BlockingQueue<String> queue1 = new ArrayBlockingQueue<>(1);
-		private final BlockingQueue<String> queue2 = new ArrayBlockingQueue<>(1);
 		private final Calculation c = new Calculation();  
 
 		public void masterToThread(String str) throws InterruptedException {
@@ -19,18 +17,6 @@ public class ThreadHandler {
 
 		public String threadFromMaster() throws InterruptedException {
 			return queue1.poll();
-		}
-
-		public String masterFromThread() throws InterruptedException {
-			return queue2.poll(10, TimeUnit.SECONDS);
-		}
-
-		public void threadToMaster(String str) throws InterruptedException {
-			queue2.put(Objects.requireNonNull(str));
-		}
-		
-		public void clearMasterFromThread() {
-			queue2.clear();
 		}
 		
 		public Calculation getCalculation() {
