c4986e4ed2d2a80d3813aba1a598e90a1a05c5da
Mannegan <iskandaroff.yenrick@gmail.com>
2018-03-06 12:20:04 +0100

    synchronized crossover, again

--- ordogene-parent/ordogene-algorithme/src/main/java/org/ordogene/algorithme/jenetics/ScheduleCrossover.java
+++ ordogene-parent/ordogene-algorithme/src/main/java/org/ordogene/algorithme/jenetics/ScheduleCrossover.java
@@ -25,104 +25,106 @@ public class ScheduleCrossover extends Crossover<ActionGene, Long> {
 
 	@Override
 	protected int crossover(MSeq<ActionGene> father, MSeq<ActionGene> mother) {
-		model.resetModel();
-		// Environment which evolve with the creation
-		Environment envAfterStart = model.getStartEnvironment().copy();
-		Environment envAfterEnd = model.getStartEnvironment().copy();
-		
-		// Map with action to stop at key value
-		SortedMap<Integer, List<Action>> map = new TreeMap<>();
-		
-		// List which represent the children sequence
-		ArrayList<ActionGene> seq = new ArrayList<>();
-		
-		// The current time
-		int timeAtStart = 0;
-		int timeAtEnd = 0;
-		
-		List<ActionGene> fatherList = father.asList();
-		boolean[] fatherActionAvailable = new boolean[father.size()];
-		List<ActionGene> motherList = mother.asList();
-		boolean[] motherActionAvailable = new boolean[mother.size()];
-
-		// Continue while a action is in progress or a Action is possible
-		while (model.hasWorkableAction(envAfterEnd, timeAtEnd) || !map.isEmpty()) {
-			int coin = flipCoin();
+		synchronized (model) {
+			model.resetModel();
+			// Environment which evolve with the creation
+			Environment envAfterStart = model.getStartEnvironment().copy();
+			Environment envAfterEnd = model.getStartEnvironment().copy();
 			
-			// SELECT AN ACTION ON A PARENT AND CHANGE ITS AVAILIBILITY ON OTHER PARENT
-			Action action = null;
-			// check father
-			if(coin == 0) {
-				action = getAction(fatherList, envAfterEnd, timeAtEnd, fatherActionAvailable);
-				if(action == null) {
-					break;
+			// Map with action to stop at key value
+			SortedMap<Integer, List<Action>> map = new TreeMap<>();
+			
+			// List which represent the children sequence
+			ArrayList<ActionGene> seq = new ArrayList<>();
+			
+			// The current time
+			int timeAtStart = 0;
+			int timeAtEnd = 0;
+			
+			List<ActionGene> fatherList = father.asList();
+			boolean[] fatherActionAvailable = new boolean[father.size()];
+			List<ActionGene> motherList = mother.asList();
+			boolean[] motherActionAvailable = new boolean[mother.size()];
+	
+			// Continue while a action is in progress or a Action is possible
+			while (model.hasWorkableAction(envAfterEnd, timeAtEnd) || !map.isEmpty()) {
+				int coin = flipCoin();
+				
+				// SELECT AN ACTION ON A PARENT AND CHANGE ITS AVAILIBILITY ON OTHER PARENT
+				Action action = null;
+				// check father
+				if(coin == 0) {
+					action = getAction(fatherList, envAfterEnd, timeAtEnd, fatherActionAvailable);
+					if(action == null) {
+						break;
+					}
+					removeAction(action , motherList, motherActionAvailable);
+				} else {
+					// check mother
+					action = getAction(motherList, envAfterEnd, timeAtEnd, motherActionAvailable);
+					if(action == null) {
+						break;
+					}
+					removeAction(action , fatherList, fatherActionAvailable);
 				}
-				removeAction(action , motherList, motherActionAvailable);
-			} else {
-				// check mother
-				action = getAction(motherList, envAfterEnd, timeAtEnd, motherActionAvailable);
-				if(action == null) {
-					break;
+	
+				
+				// Parallele start
+				if(model.workable(action, envAfterStart, timeAtStart)) {
+					// Create ActionGene and add it on seq
+					ActionGene actionGene = ActionGene.of(action, timeAtStart);
+					seq.add(actionGene);
+	
+					//Change StartEnvironment
+					model.startAction(action, envAfterStart, timeAtStart);
+					
+				} else {
+				// start After
+					ActionGene actionGene = ActionGene.of(action, timeAtEnd);
+					seq.add(actionGene);
+					
+					// Change startTime
+					timeAtStart = timeAtEnd;
+					
+					// Change StartEnvironment
+					envAfterStart = envAfterEnd;
+					model.startAction(action, envAfterStart, timeAtStart);
 				}
-				removeAction(action , fatherList, fatherActionAvailable);
-			}
-
-			
-			// Parallele start
-			if(model.workable(action, envAfterStart, timeAtStart)) {
-				// Create ActionGene and add it on seq
-				ActionGene actionGene = ActionGene.of(action, timeAtStart);
-				seq.add(actionGene);
-
-				//Change StartEnvironment
-				model.startAction(action, envAfterStart, timeAtStart);
 				
+				// Calcul actionEndTime
+				int actionEndTime = timeAtStart + action.getTime();
+				// Add action in map to end it later
+				List<Action> actions = map.get(actionEndTime);
+				if (actions == null) {
+					actions = new ArrayList<>();
+					map.put(actionEndTime, actions);
+				}
+				actions.add(action);
+				
+				if(timeAtEnd == timeAtStart) {
+					// Remove action which ended in previous startTime
+					map.remove(timeAtEnd);
+				}
+				
+				// Change timeAtEnd
+				timeAtEnd = map.firstKey();
+				
+				// Change EndEnvironment
+				envAfterEnd = envAfterStart.copy();
+				for(Action a : map.get(timeAtEnd)) {
+					model.endAction(envAfterEnd, a);
+				}
+				
+			}
+			
+			if(flipCoin() == 0) {
+				father = MSeq.of(seq);
 			} else {
-			// start After
-				ActionGene actionGene = ActionGene.of(action, timeAtEnd);
-				seq.add(actionGene);
-				
-				// Change startTime
-				timeAtStart = timeAtEnd;
-				
-				// Change StartEnvironment
-				envAfterStart = envAfterEnd;
-				model.startAction(action, envAfterStart, timeAtStart);
+				mother = MSeq.of(seq);
 			}
 			
-			// Calcul actionEndTime
-			int actionEndTime = timeAtStart + action.getTime();
-			// Add action in map to end it later
-			List<Action> actions = map.get(actionEndTime);
-			if (actions == null) {
-				actions = new ArrayList<>();
-				map.put(actionEndTime, actions);
-			}
-			actions.add(action);
-			
-			if(timeAtEnd == timeAtStart) {
-				// Remove action which ended in previous startTime
-				map.remove(timeAtEnd);
-			}
-			
-			// Change timeAtEnd
-			timeAtEnd = map.firstKey();
-			
-			// Change EndEnvironment
-			envAfterEnd = envAfterStart.copy();
-			for(Action a : map.get(timeAtEnd)) {
-				model.endAction(envAfterEnd, a);
-			}
-			
+			return 1;
 		}
-		
-		if(flipCoin() == 0) {
-			father = MSeq.of(seq);
-		} else {
-			mother = MSeq.of(seq);
-		}
-		
-		return 1;
 	}
 
 	private void removeAction(Action actionToRemove, List<ActionGene> list, boolean[] actionAvailable) {
