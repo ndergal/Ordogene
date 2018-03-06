ac0c705c2ec7e4d337c99a6ae8ea73317a81923b
Mannegan <iskandaroff.yenrick@gmail.com>
2018-03-06 12:18:14 +0100

    oho

--- ordogene-parent/ordogene-api/src/test/java/org/ordogene/api/CalculationControllerTest.java
+++ ordogene-parent/ordogene-api/src/test/java/org/ordogene/api/CalculationControllerTest.java
@@ -377,10 +377,6 @@ public class CalculationControllerTest {
 	@Test
 	public void removeCalculationUserNotExistTest() throws Exception {
 		FileUtils.removeUser(userTest);
-<<<<<<< HEAD
-
-=======
->>>>>>> branch 'dev' of https://ndergal@bitbucket.org/darwinners/ordogene.git
 		
 		mvc.perform(delete("/" + userTest + "/calculations/" + cidTest)).andExpect(status().isNotFound())
 				.andReturn();
